33929fc8298317a990bbf9e25310016b93c49c1b
Keroll322 <kirillbilay322@gmail.com>
2020-11-16 17:53:57 +0500

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,28 @@
+public class Variable {
+	public static void main(String[] args) {
+	/*
+		Intel® Core™ i5-4440 3.10 GHz
+		GeForce® GTX 960 4gb
+		8gb RAM memory
+	*/
+
+	byte b = 5;
+	short s = 4440;
+	int i = 960;
+	long l = 4L;
+	float f = 3.10F;
+	double d = 2.5D;
+	char c = '\u003F';
+	boolean b1 = true;
+
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(i);
+	System.out.println(l);
+	System.out.println(f);
+	System.out.println(d);
+	System.out.println(c);
+	System.out.println(b1);
+
+	}
+}
