3e8c2eb4ebc92257e18a5dad6567298b0a4aa88a
Keroll322 <kirillbilay322@gmail.com>
2020-11-22 13:09:51 +0500

    HW_2changes

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,86 @@
+public class Jaeger {
+	private String name;
+	private String mark;
+	private String origin;
+	private double height;
+	private double weight;
+	private int speed;
+	private int strenght;
+	private int armor;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public void setStrenght(int strenght) {
+		this.strenght = strenght;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public int getStrenght() {
+		return strenght;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	public void run() {
+		System.out.println("Вы бежите");
+	}
+
+	public void jump() {
+		System.out.println("Вы прыгаете");
+	}
+
+	public void shoot() {
+		System.out.println("Вы стреляете");
+	}
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,31 @@
+public class JaegerTest {
+	public static void main(String[] args) {
+		Jaeger jaeger1 = new Jaeger();
+		Jaeger jaeger2 = new Jaeger();
+
+		jaeger1.setName("Gipsy Danger");
+		jaeger2.setName("Cherno Alpha");
+		jaeger1.setMark("Mark-3");
+		jaeger2.setMark(jaeger1.getMark());
+		jaeger1.setOrigin("USA");
+		jaeger2.setOrigin("Russia");
+		jaeger1.setHeight(260);
+		jaeger2.setHeight(280);
+		jaeger1.setWeight(1.9);
+		jaeger2.setWeight(2.4);
+		jaeger1.setSpeed(7);
+		jaeger2.setSpeed(3);
+		jaeger1.setStrenght(8);
+		jaeger2.setStrenght(10);
+		jaeger1.setArmor(6);
+		jaeger2.setArmor(jaeger1.getArmor());
+
+		System.out.println("name jaeger1 = " + jaeger1.getName());
+		System.out.println("name jaeger2 = " + jaeger2.getName());
+		System.out.println("armor jaeger1 = " + jaeger1.getArmor());
+		System.out.println("armor jaeger2 = " + jaeger2.getArmor());
+
+		jaeger1.run();
+		jaeger2.shoot();
+	}
+}
