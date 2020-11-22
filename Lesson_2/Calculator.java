6ca66c1e69fe08cfba46e30f5f53a6c7fbe118e6
Keroll322 <kirillbilay322@gmail.com>
2020-11-22 11:37:19 +0500

    HW_2changes

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,47 @@
+public class Calculator {
+	private int firstNumber;
+	private int secondNumber;
+	private char mathOperation;
+
+	public void setFirstNumber(int firstNumber) {
+		this.firstNumber = firstNumber;
+	}
+
+	public void setSecondNumber(int secondNumber) {
+		this.secondNumber = secondNumber;
+	}
+
+	public void setMathOperation(char mathOperation) {
+		this.mathOperation = mathOperation;
+	}
+
+	public int calcul() {
+		switch(mathOperation) {
+			case '+':
+				System.out.println(firstNumber + secondNumber);
+				break;
+			case '-':
+				System.out.println(firstNumber - secondNumber);
+				break;
+			case '/':
+				System.out.println(firstNumber / secondNumber);
+				break;
+			case '*':
+				System.out.println(firstNumber * secondNumber);
+				break;
+			case '%':
+				System.out.println(firstNumber % secondNumber);
+				break;
+			case '^':
+				int result = 1;
+				for(int i = 1; i <= secondNumber; i++) {
+					result *= firstNumber;
+				}
+				System.out.println(result);
+				break;
+			default:
+				System.out.println("Не верный математический знак");
+		}
+		return 0;
+	}
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CalculatorTest {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Введите первое число: ");
+		calc.setFirstNumber(sc.nextInt());
+
+		System.out.println("Введите знак математической операции: ");
+		calc.setMathOperation(sc.next().charAt(0));
+
+		System.out.println("Введите второе число: ");
+		calc.setSecondNumber(sc.nextInt());
+
+		System.out.println(calc.calcul());
+	}
+}
